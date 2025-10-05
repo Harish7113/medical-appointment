9eac753b1640ccc43659b7a7fc73016279b80d21
Harishprasanth kalidas <harishkalidas007@gmail.com>
2025-10-05 22:36:07 +0530

    Initial commit - medical-appointment-app

--- frontend/src/api/doctorsApi.js
+++ frontend/src/api/doctorsApi.js
@@ -1,34 +1,8 @@
 import axios from "axios";
 
-const API_URL = `${process.env.REACT_APP_API_URL}/api/doctors`;
+const API_URL = `${process.env.REACT_APP_API_URL}/api/doctors`; // <-- make sure this points to deployed backend
 
-
-// Get all doctors
-export const getDoctors = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-};
-
-// Get doctor by ID
-export const getDoctorById = async (id) => {
-  const response = await axios.get(`${API_URL}/${id}`);
-  return response.data;
-};
-
-// Add a new doctor
-export const addDoctor = async (doctor) => {
-  const response = await axios.post(API_URL, doctor);
-  return response.data;
-};
-
-// Update a doctor
-export const updateDoctor = async (id, updatedDoctor) => {
-  const response = await axios.put(`${API_URL}/${id}`, updatedDoctor);
-  return response.data;
-};
-
-// Delete a doctor
-export const deleteDoctor = async (id) => {
-  const response = await axios.delete(`${API_URL}/${id}`);
-  return response.data;
-};
+export const getDoctors = () => axios.get(API_URL).then(res => res.data);
+export const addDoctor = (doctor) => axios.post(API_URL, doctor).then(res => res.data);
+export const updateDoctor = (id, doctor) => axios.put(`${API_URL}/${id}`, doctor).then(res => res.data);
+export const deleteDoctor = (id) => axios.delete(`${API_URL}/${id}`).then(res => res.data);
